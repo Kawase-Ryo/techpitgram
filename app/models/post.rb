10a70201fc173dbd3c34f090ca1c987b0438106e
Kawase-Ryo <2801r.kawase@gmail.com>
2022-08-21 08:09:12 +0900

    いいね機能

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,14 @@ class Post < ApplicationRecord
     belongs_to :user
     has_many :photos, dependent: :destroy
 
+    has_many :likes, -> { order(created_at: :desc) }, dependent: :destroy
+    #dependent: :destroy投稿が削除されたら、その投稿に紐づくいいねも削除
+
     accepts_nested_attributes_for :photos
+
+    def liked_by(user)
+        # user_idとpost_idが一致するlikeを検索する
+        Like.find_by(user_id: user.id, post_id: id)
+    end
     
 end

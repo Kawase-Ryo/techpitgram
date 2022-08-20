e8382640cd4ec2386b56232f19f58c330a475183
Kawase-Ryo <2801r.kawase@gmail.com>
2022-08-21 08:40:12 +0900

    プロフィール・ユーザーネーム編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
 
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :username])
-     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:name, :username])
     end
     #nameカラムを保存できるようにする記述はサインアップやアカウントをアップデートするときだけにしか必要
 
